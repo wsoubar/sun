559362c639f03fb93acf7b095c4e1f3655a5f373
Wagner Barbosa <wsoubar@gmail.com>
2025-08-31 18:51:17 -0300

    inicial

---
+++ src/main/java/br/com/wsoubar/sun/model/Author.java
@@ -0,0 +1,19 @@
+package br.com.wsoubar.sun.model;
+
+public class Author {
+    private String name;
+    private String email;
+
+    public Author(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
